adcd86d815d234efcbe01bc7e2fef47c0e502f42
developeroliver <developpeur.olive@gmail.com>
2024-04-18 17:52:08 +0200

    create loginView and loginVC

--- Bankey/Controller/LoginVC.swift
+++ Bankey/Controller/LoginVC.swift
@@ -11,9 +11,55 @@ import SnapKit
 class LoginVC: UIViewController {
     
     // MARK: - Properties
-    
-    // MARK: - UI Declarations
     private let loginView = LoginView()
+    var username: String? {
+        return loginView.usernameTextField.text
+    }
+    
+    var password: String? {
+        return loginView.passwordTextField.text
+    }
+    // MARK: - UI Declarations
+    let textLabel: UILabel = {
+        let label = UILabel()
+        label.text = "label"
+        return label
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+        label.adjustsFontForContentSizeCategory = true
+        label.text = "Bankey"
+        return label
+    }()
+    private let subtitleLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont.preferredFont(forTextStyle: .title3)
+        label.adjustsFontForContentSizeCategory = true
+        label.numberOfLines = 0
+        label.text = "Votre source privilégiée pour tout ce qui touche à la banque!"
+        return label
+    }()
+    
+    private let signInButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.configuration = .filled()
+        button.configuration?.imagePadding = 8
+        button.setTitle("Se connecter", for: .normal)
+        return button
+    }()
+    
+    private let errorMessageLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.textColor = .systemRed
+        label.numberOfLines = 0
+        label.isHidden = true
+        return label
+    }()
     
     // MARK: - LifeCycle Methods
     override func viewDidLoad() {
@@ -21,27 +67,90 @@ class LoginVC: UIViewController {
         setup()
         layout()
     }
-    
-    
 }
 
 // MARK: - @objc Functions
+extension LoginVC {
+    
+    @objc func signInTapped() {
+        errorMessageLabel.isHidden = true
+        login()
+    }
+}
+
+// MARK: - Logic Functions
+extension LoginVC {
+    
+    private func login() {
+        guard let username = username, let password = password else {
+            assertionFailure("Utilisateur / mot de passe ne doit pas être nul")
+            return
+        }
+        
+        if username.isEmpty || password.isEmpty {
+            configureView(withMessage: "Utilisateur / mot de passe ne peut être vide")
+            return
+        }
+        
+        if username == "" && password == "" {
+            signInButton.configuration?.showsActivityIndicator = true
+        } else {
+            configureView(withMessage: "Utilisateur / mot de passe incorrect")
+        }
+    }
+    
+    private func configureView(withMessage message: String) {
+        errorMessageLabel.isHidden = false
+        errorMessageLabel.text = message
+    }
+}
 
 // MARK: - Helpers
 extension LoginVC {
     
     private func setup() {
         view.backgroundColor = .systemBackground
+        
+        signInButton.addTarget(self, action: #selector(signInTapped), for: .touchUpInside)
     }
     
     private func layout() {
         view.addSubview(loginView)
+        view.addSubview(subtitleLabel)
+        view.addSubview(titleLabel)
+        view.addSubview(signInButton)
+        view.addSubview(errorMessageLabel)
+        
+        titleLabel.snp.makeConstraints { make in
+            make.bottom.equalTo(subtitleLabel.snp.top).offset(-24)
+            make.leading.equalToSuperview().offset(8)
+            make.trailing.equalToSuperview().offset(-8)
+        }
+        
+        subtitleLabel.snp.makeConstraints { make in
+            make.bottom.equalTo(loginView.snp.top).offset(-24)
+            make.leading.equalToSuperview().offset(8)
+            make.trailing.equalToSuperview().offset(-8)
+        }
         
         loginView.snp.makeConstraints { make in
             make.centerY.equalToSuperview()
             make.leading.equalToSuperview().offset(8)
             make.trailing.equalToSuperview().offset(-8)
+            make.height.equalTo(80)
         }
         
+        signInButton.snp.makeConstraints { make in
+            make.top.equalTo(loginView.snp.bottom).offset(16)
+            make.leading.equalToSuperview().offset(8)
+            make.trailing.equalToSuperview().offset(-8)
+            make.height.equalTo(40)
+        }
+        
+        errorMessageLabel.snp.makeConstraints { make in
+            make.top.equalTo(signInButton.snp.bottom).offset(16)
+            make.leading.equalToSuperview().offset(8)
+            make.trailing.equalToSuperview().offset(-8)
+        }
     }
 }
--- Bankey/View/LoginView.swift
+++ Bankey/View/LoginView.swift
@@ -9,34 +9,93 @@ import UIKit
 
 class LoginView: UIView {
     
+    // MARK: - UI Declarations
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [
+            usernameTextField,
+            dividerView,
+            passwordTextField,
+        ])
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        return stackView
+    }()
+
+    let usernameTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Entrer un nom d'utilisateur"
+        return tf
+    }()
+    
+    let passwordTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Entrer votre mot de passe"
+        tf.isSecureTextEntry = true
+        return tf
+    }()
+    
+    private let dividerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .secondarySystemFill
+        return view
+    }()
+    
+        
+    // MARK: - LifeCycle Methods
     override init(frame: CGRect) {
         super.init(frame: frame)
         style()
         layout()
     }
     
-    
-    override var intrinsicContentSize: CGSize {
-        return CGSize(width: 200, height: 200)
-    }
-    
-    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
 
-
+// MARK: - Helpers
 extension LoginView {
     
-    private func style() {
-        backgroundColor = .systemPink
+    private func style() {        
+        backgroundColor = .secondarySystemBackground
+        layer.cornerRadius = 5
+        clipsToBounds = true
+        
+        usernameTextField.delegate = self
+        passwordTextField.delegate = self
     }
     
     
     private func layout() {
+        addSubview(stackView)
+        stackView.snp.makeConstraints { make in
+            make.top.leading.equalToSuperview().offset(8)
+            make.bottom.equalToSuperview().offset(-8)
+            make.trailing.equalToSuperview().offset(-8)
+        }
+        
+        dividerView.snp.makeConstraints { make in
+            make.height.equalTo(1)
+        }
         
     }
     
 }
 
+// MARK: - UITextFieldDelegate
+extension LoginView: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        usernameTextField.endEditing(true)
+        passwordTextField.endEditing(true)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+    }
+}
